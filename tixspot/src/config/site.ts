1af9fb02e7a10593e3263c31061896586e31eb0f
Karunamay Murmu <karunamaymurmu.km@gmail.com>
2023-07-27 09:48:56 +0530

    configure eslint and prettier. Fix existing eslint code style issues.

--- tixspot/src/config/site.ts
+++ tixspot/src/config/site.ts
@@ -1,18 +1,17 @@
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
-  links: {
-    twitter: "",
-    github: "",
-    docs: "",
-  },
-}
+	name: "Next.js",
+	description: "",
+	mainNav: [
+		{
+			title: "Home",
+			href: "/",
+		},
+	],
+	links: {
+		twitter: "",
+		github: "",
+		docs: "",
+	},
+};
--- tixspot/src/pages/_app.tsx
+++ tixspot/src/pages/_app.tsx
@@ -5,15 +5,15 @@ import { SiteHeader } from "@/components/site-header";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <div className="relative flex min-h-screen flex-col">
-        <SiteHeader />
-        <div className="flex-1">
-          <Component {...pageProps} />
-        </div>
-      </div>
-      <TailwindIndicator />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+			<div className="relative flex min-h-screen flex-col">
+				<SiteHeader />
+				<div className="flex-1">
+					<Component {...pageProps} />
+				</div>
+			</div>
+			<TailwindIndicator />
+		</ThemeProvider>
+	);
 }
